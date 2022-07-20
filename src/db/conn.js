0bebd55b67526af53f8941aacaa234108d52a93b
jessica banka <jessicabanka3081@gmail.com>
2022-07-20 23:20:46 +0530

    delete

--- src/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Registration",{
-   
-
-}).then(() =>{
-    console.log(`connection successful`)
-}).catch((e) =>{
-    console.log(`no connection`);
-})
